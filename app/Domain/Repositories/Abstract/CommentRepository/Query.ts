7d8e62e4ea37ea60835737241504cf1d7c69871b
Yashika <yashikabaan096@gmail.com>
2023-06-19 13:21:45 +0900

    Respository queryies

---
+++ app/Domain/Repositories/Abstract/CommentRepository/Query.ts
@@ -0,0 +1,100 @@
+import { Query as DatasourceQuery } from "App/Data/Datasources/Abstract/CommentDatasource/Query";
+import { DateTime } from "luxon";
+
+export class Query {
+    public id: string | null = null;
+    public ids: string[] | null = null;
+    public notIds: string[] | null = null;
+
+    public userId: string | null = null;
+    public postId: string | null = null;
+    public postIds: string[] | null = null;
+    public notPostIds: string[] | null = null;
+    public comment: string | null = null;
+
+    public minCreatedAt: DateTime | null = null;
+    public maxCreatedAt: DateTime | null = null;
+    public minUpdatedAt: DateTime | null = null;
+    public maxUpdatedAt: DateTime | null = null;
+
+    public offset = 0;
+    public limit = 60;
+    public sort = "created_at:desc";
+
+    public static fromOptions(options?: { [key: string]: any }) {
+        return new this(options);
+    }
+
+    constructor(options?: { [key: string]: any }) {
+        if (options === undefined) {
+            return;
+        }
+        if (options["id"] !== undefined) {
+            this.id = options["id"];
+        }
+        if (options["ids"] !== undefined) {
+            this.ids = options["ids"];
+        }
+        if (options["notIds"] !== undefined) {
+            this.notIds = options["notIds"];
+        }
+        if (options["userId"] !== undefined) {
+            this.userId = options["userId"];
+        }
+        if (options["postId"] !== undefined) {
+            this.postId = options["postId"];
+        }
+        if (options["postIds"] !== undefined) {
+            this.postIds = options["postIds"];
+        }
+        if (options["notPostIds"] !== undefined) {
+            this.notPostIds = options["notPostIds"];
+        }
+        if (options["comment"] !== undefined) {
+            this.comment = options["comment"];
+        }
+        if (options["minCreatedAt"] !== undefined) {
+            this.minCreatedAt = options["minCreatedAt"];
+        }
+        if (options["maxCreatedAt"] !== undefined) {
+            this.maxCreatedAt = options["maxCreatedAt"];
+        }
+        if (options["minUpdatedAt"] !== undefined) {
+            this.minUpdatedAt = options["minUpadtedAt"];
+        }
+        if (options["maxUpdatedAt"] !== undefined) {
+            this.maxUpdatedAt = options["maxUpadtedAt"];
+        }
+
+        if (options["offset"] != undefined) {
+            this.offset = options["offset"];
+        }
+        if (options["limit"] != undefined) {
+            this.limit = options["limit"];
+        }
+        if (options["sort"] != undefined) {
+            this.sort = options["sort"];
+        }
+    }
+
+    public toDatasourceQuery(): DatasourceQuery {
+        const query = new DatasourceQuery();
+        query.id = this.id;
+        query.ids = this.ids;
+        query.notIds = this.notIds;
+        query.userId = this.userId;
+        query.postId = this.postId;
+        query.postIds = this.postIds;
+        query.notPostIds = this.notPostIds;
+        query.comment = this.comment;
+        query.minCreatedAt = this.minCreatedAt;
+        query.maxCreatedAt = this.maxCreatedAt;
+        query.minUpdatedAt = this.minUpdatedAt;
+        query.maxUpdatedAt = this.maxUpdatedAt;
+        query.offset = this.offset;
+        query.limit = this.limit;
+        query.sort = this.sort;
+
+        return query;
+    }
+}
